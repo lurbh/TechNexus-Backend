be44142849c83a34bc44efd72a20ad02ee04e005
lurbh <lurbh@hotmail.com>
2024-05-13 11:02:08 +0000

    for render

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -5,8 +5,7 @@ const knex = require("knex")({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    'ssl': true
+    host: process.env.DB_HOST
   },
 });
 const bookshelf = require("bookshelf")(knex);
